41c89d2e005b2a1db47f6617882ff756b599c022
Artem Bespolov <artembespolov@Artems-MacBook-Pro.local>
2022-07-20 22:17:38 +0300

    fix

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import s from "./Main.module.css";
 import Particles from "react-tsparticles";
 import {ISourceOptions} from 'tsparticles-engine';
+import { loadFull } from "tsparticles";
 
 const particlesOptions: ISourceOptions = {
     fpsLimit: 60,
     interactivity: {
         events: {
-            onClick: { enable: true, mode: "push" },
+            onClick: {enable: true, mode: "push"},
             onHover: {
                 enable: true,
                 mode: "repulse",
-                parallax: { enable: false, force: 60, smooth: 10 }
+                parallax: {enable: false, force: 60, smooth: 10}
             },
             resize: true
         },
         modes: {
-            push: { quantity: 4 },
-            repulse: { distance: 200, duration: 0.4 }
+            push: {quantity: 4},
+            repulse: {distance: 200, duration: 0.4}
         }
     },
     particles: {
-        color: { value: "#ffffff" },
+        color: {value: "#ffffff"},
         move: {
             direction: "none",
             enable: true,
@@ -55,17 +56,108 @@ const particlesOptions: ISourceOptions = {
             type: "circle"
         },
         size: {
-            value: { min: 1, max: 5 }
+            value: {min: 1, max: 5}
         }
     }
 }
 
 
+const Main = () => {
+    //@ts-ignore
+    const particlesInit = async (main) => {
+        console.log(main);
 
-        const Main = () => {
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    //@ts-ignore
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
     return (
         <div className={s.mainBlock}>
-            <Particles className={s.particles} options={particlesOptions}/>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded as any}
+                options={{
+                    fullScreen: false,
+                    background: {
+                        color: {
+                            value: "#0d47a1"
+                        }
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push"
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff"
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1
+                        },
+                        collisions: {
+                            enable: true
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce"
+                            },
+                            random: false,
+                            speed: 6,
+                            straight: false
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800
+                            },
+                            value: 80
+                        },
+                        opacity: {
+                            value: 0.5
+                        },
+                        shape: {
+                            type: "circle"
+                        },
+                        size: {
+                            value: { min: 1, max: 5 }
+                        }
+                    },
+                    detectRetina: true
+                }}
+            />
             <div className={s.container}>
                 <div className={s.mainGreeting}>
                     <span>Hi there</span>
