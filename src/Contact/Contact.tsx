f421eba5c90712d6fd97fe007fa916f1fbe80335
VladBula <vladbulynk@gmail.com>
2022-08-11 14:55:58 +0300

    deploy heroku

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import s from "./Contact.module.css";
+import axios from "axios";
 
 const Contact = () => {
+
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
+
+
+    const changeNameHandler = (e: FormEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+    const changeEmailHandler = (e: FormEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+    const changeMessageHandler = (e: FormEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+
+    const sendMessage = async () => {
+            try {
+                await axios.post('https://https://smtp-node-serv.herokuapp.com//sendMessage', {name, email, message})
+            } catch (e) {
+                alert('Some error')
+            }
+        }
+
     return (
         <div className={s.contactBlock} id="contacts">
             <div className={s.container}>
@@ -9,18 +34,15 @@ const Contact = () => {
                     <h2 className={s.title}>Contacts</h2>
                     <div className={s.line}></div>
                 </div>
-                <form className={s.form} action="">
-                    <input type="text" placeholder="Name"/>
-                    <input type="text" placeholder="E-mail"/>
-                    <textarea placeholder="Your message"></textarea>
+                <form className={s.form}>
+                    <input type="text" placeholder="Name" onClick={changeNameHandler}/>
+                    <input type="email" placeholder="E-mail" onClick={changeEmailHandler}/>
+                    <textarea placeholder="Your message" onClick={changeMessageHandler}></textarea>
                 </form>
                 <div className={s.contactButton}>
-                    <a href="#">Send</a>
+                    <button type='submit' onClick={sendMessage}>Send</button>
                 </div>
-
-
             </div>
-
         </div>
     );
 };
